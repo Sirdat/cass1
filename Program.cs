fbca891c261d34e85c98520f12804bbdcf901d02
Sirdat <88361775+Sirdat@users.noreply.github.com>
2022-01-10 19:04:42 +0700

    Add files via upload
    h

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+        }
+    }
+}
